7eeaec161e3f8085de7ab75ee611676eafaffc7b
geiselrocha <geiselrocha@hotmail.com>
2022-11-10 23:11:31 -0300

    Requisito 23

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -11,8 +11,6 @@ db.produtos.updateOne(
 db.produtos.updateMany({
   tags: { $elemMatch: { $in: ["bovino"] } },
 },
-  { $inc: { "vendasPorDia.6": 120 } },
-);
+  { $inc: { "vendasPorDia.6": 120 } });
 
-db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 },
-);
+db.produtos.find({}, { nome: 1, vendasPorDia: 1, _id: 0 });
